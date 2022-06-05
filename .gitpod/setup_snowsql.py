ba69253ce523493cb225515e001465dbb5a53146
Jonathan Merlevede <jonathan@merlevede.eu>
2022-06-05 16:28:21 +0000

    Set snowsql config on start

---
+++ .gitpod/setup_snowsql.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from configparser import ConfigParser
+import os
+
+config_dir = os.path.expanduser("~/.snowsql")
+if not os.path.exists(config_dir):
+    os.mkdir(config_dir)
+config_path = os.path.expanduser("~/.snowsql/config")
+config_config = ConfigParser()
+config_config.read(config_path)  # if path does not exist, this is no-op
+
+
+if "connections" not in config_config:
+    config_config.add_section("connections")
+
+config_config.set("connections", "accountname", os.getenv("SNOWFLAKE_ACCOUNT_NAME"))
+config_config.set("connections", "region", os.getenv("SNOWFLAKE_REGION"))
+config_config.set("connections", "username", os.getenv("SNOWFLAKE_USER"))
+config_config.set("connections", "password", os.getenv("SNOWFLAKE_PASSWORD"))
+config_config.set("connections", "dbname", os.getenv("SNOWFLAKE_DEFAULT_DB"))
+config_config.set("connections", "schemaname", os.getenv("SNOWFLAKE_DEFAULT_SCHEMA"))
+config_config.set("connections", "warehousename", os.getenv("SNOWFLAKE_DEFAULT_WAREHOUSE"))
+config_config.set("connections", "rolename", os.getenv("SNOWFLAKE_DEFAULT_ROLE"))
+
+with open(config_path, "w") as f:
+    config_config.write(f)
