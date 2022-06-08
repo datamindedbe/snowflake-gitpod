b9c3004a0d7792486fa3d521b5f9e8ca4a071ca6
Jonathan Merlevede <jonathan@merlevede.eu>
2022-06-08 15:51:40 +0000

    script fix

--- .gitpod/setup_environment.sh
+++ .gitpod/setup_environment.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
 vars=("SNOWFLAKE_ORGANISATION_NAME" "SNOWFLAKE_ACCOUNT_NAME" "SNOWFLAKE_USER" "SNOWFLAKE_PASSWORD" "SNOWFLAKE_DEFAULT_ROLE" "SNOWFLAKE_DEFAULT_WAREHOUSE" "SNOWFLAKE_DEFAULT_DB" "SNOWFLAKE_DEFAULT_SCHEMA")
 
 for var in ${vars[@]}; do
-    if [ -z ${!var} ]; then
-        [ $has_slept ] || { sleep 4; has_slept=1; }
+    if [ -z "${!var}" ]; then
+        [ $has_slept ] || { read -p "Press enter to continue"; has_slept=1; }
         read -p "Please provide a value for ${var}: " myval
         # Ensure that variables are exported in this shell
-        declare -x "${var}=${myval}"
+        # declare -x "${var}=${myval}"
         # Ensure that variables will be persisted in future Gitpod workspaces
         gp env "${var}=${myval}" > /dev/null
         # Ensure that variables will be available in other shells in this workspace
