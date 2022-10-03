c0dbc39e65afe057bd81a79f070f87ae3aff3594
Jonathan Merlevede <jonathan@merlevede.eu>
2022-10-03 21:29:50 +0000

    add comments to venv_run

--- .pre-commit/venv_run.sh
+++ .pre-commit/venv_run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 p="$(cd "$(dirname "$0")"; pwd)"
-cd "$p/.."
+cd "$p/.." || exit
+# activate virtual environment if it is not already activated
 if python -c "import sys; exit(sys.prefix != sys.base_prefix)"; then
     source .venv/bin/activate
 fi
+# first argument to this script is the command; execute it,
+# and pass to it all additional arguments passed to this script, if any
 cmd=$1
 shift
 exec "$cmd" "$@"
