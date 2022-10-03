8a1e7e9e3d7f0cc1af61389fd26331a3925abd07
Jonathan Merlevede <jonathan@merlevede.eu>
2022-10-03 21:43:23 +0000

    ...

--- .gitpod/setup_environment.sh
+++ .gitpod/setup_environment.sh
@@ -6,10 +6,11 @@ vars=(\
     "SNOWSQL_PWD"
 )
 
+read -r -p "Press enter to configure snowsql"
 for var in "${vars[@]}"; do
     if [ -z "${!var}" ]; then
         [ "$has_slept" ] || { read -r -p "Press enter to continue"; has_slept=1; }
-        read -r -p "Please provide a value for ${var}: " myval
+        read -r -p "Please provide a value for ${var} [default:${var:-none}]: " myval
         # Ensure that variables are exported in this shell
         declare -x "${var}=${myval}"
         # Ensure that variables will be persisted in future Gitpod workspaces
